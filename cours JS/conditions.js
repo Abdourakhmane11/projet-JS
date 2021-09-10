7e20e0a510dfce75f948694886264a5d007eb480
Abdourakhmane11 <88661157+Abdourakhmane11@users.noreply.github.com>
2021-09-10 10:31:32 +0000

    Add files via upload

---
+++ cours JS/conditions.js
@@ -0,0 +1,119 @@
+//let exp = 2 ;
+//if ( exp > 1 )
+//{
+//console.log ("resultat" + exp);
+//};
+
+/*let age = 1 ;
+if (age <= 18)
+{
+    console.log(" vous etes mineur " );
+}
+else if (age > 18  &&  age < 35)
+{
+    console.log("vous etes adulte");
+}
+else
+{
+    console.log("vous etes vieux");
+}*/
+
+/*let i=0
+for ( i ; i > 10 ; i++ ) // i++ = i= i + 1
+{
+    console.log("bonjour");
+}*/
+
+/*for ( let i = 0; i < 100 ; i++) // for comprend 3 paties : initialisation , condition, incremantation
+{
+    console.log(i);
+}*/
+
+/*let age = 155
+if ( age > 1  &&  age <= 6 ) 
+{
+    console.log("vous etes un jeune enfant")
+}
+else if ( age >7 &&  age <11 )
+{
+    console.log("vous etes un enfant qu'a atteint l'age de raison")
+}
+else if (age >12 && age < 17 )
+{
+    console.log("vous etes ado")
+}
+else ( age < 120)
+{
+    console.log("vous etes adulte")
+}*/
+
+/*let saisi = 2  la table de 2
+for (let i = 1; i <= 10; i++)
+    {
+        console.log(" 2 * "+ i + " = " + saisi * i)
+    }*/
+
+/*for (let i = 1; i <= 10; i++) 
+      {
+          console.log (i + " = " +i +1) ;
+      }*/
+
+      // les fonctions
+
+     /* function carre ( )
+      {
+          let x=2;
+           console.log(x*x);
+      }
+      carre (2)*/
+
+      /*function test ( x,y)
+      {
+          console.log( (x*x) + x + y)
+      }
+      test ( 2,4)*/
+     /* function table(x)
+      {
+          for ( let i=0; i<=10; i++)
+          console.log(x + "*" + i + "=" + x * i);
+      }
+     table (10)*/
+
+    /* switch (5)
+     {
+    case 1: 
+         console.log("tiroir 1: livre , stylo")
+         break;
+    case 2:
+        console.log( "tiroir 2: souris, clavier")   
+        break;
+    case 3:
+        console.log( " chaussures, casquette") 
+        break;
+    case 4: 
+         console.log( "chemis , pantalon")   
+         break;
+         default:
+         console.log("le meuble ne contient que 4 tiroirs")  
+         break;    
+     }*/
+      //decrementation
+      let i = 10 
+for (i = 10; i>=1; i--)
+    {
+        console.log(i)
+    }
+//incrementation
+   /* let i = 1
+    for ( i =1 ; i <= 10 ; i ++)
+    {
+        console.log(i)
+    }*/
+
+   /*var i= 1;
+   do 
+   {
+      alet(1); 
+     i= i+1  
+   }  while ( i <= 10)
+  */
---
+++ cours JS/test.js
@@ -0,0 +1,13 @@
+console.log(false && false)
+console.log(false && false)
+console.log(true && false)
+console.log(true && true)
+//operateur ou ||
+console.log(false||false)
+console.log(false||true)
+console.log(true||false)
+console.log(true||true)
+console.log(42<42)||(42===42);
+console.log(42<42)&&(42===42);
+
+console.log((150 >= 120  || 16 > 15) && (95 < 90 ));
